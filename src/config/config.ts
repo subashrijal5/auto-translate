1ad6744518f1b91105dfcb04d32ef2d01727054f
subash rijal <43545552+subashrijal5@users.noreply.github.com>
2024-04-08 21:31:49 +0900

    feat: adds custom configuratons

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,30 @@
 import { ConfigType } from "../types/config";
 
+export function getFileMap(){
+	const fileMapEnv = process.env.LANGUAGE_CODE_MAP;
+	const fileMapSplit = (fileMapEnv ?? "").split(",");
+	const fileMap = fileMapSplit.reduce((acc: Record<string, string>, curr) => {
+		const [key, value] = curr.split(":");
+		acc[key] = value;
+		return acc;
+	}, {});
+	return fileMap;
+}
+const fileMap = getFileMap();
+export function getLocaleCodeMapFromServer(langCode: string){
+	const locale = Object.keys(fileMap).find((key)=> fileMap[key] === langCode);
+	return locale ?? langCode;
+}
+
 export const config: ConfigType = {
-	LOCALIZE_API_KEY: process.env.LOCALIZE_API_KEY,
+	BILINGUAL_API_KEY: process.env.BILINGUAL_API_KEY,
 	LOCALE_BASE_PATH: process.env.LOCALE_BASE_PATH ?? "/locales",
-	LOCALIZE_API_URL:
-    process.env.LOCALIZE_API_URL ?? "https://langlift.bhudex.com",
+	BILINGUAL_API_URL:
+    process.env.BILINGUAL_API_URL ?? "https://bilingualapp.vuvusha.com",
 	GROUPED: process.env.GROUPED
 		? (process.env.GROUPED as "true" | "false")
 		: "true",
-	LOCALIZE_DEFAULT_FILE_NAME:
-    process.env.LOCALIZE_DEFAULT_FILE_NAME ?? "default",
+	BILINGUAL_DEFAULT_FILE_NAME:
+    process.env.BILINGUAL_DEFAULT_FILE_NAME ?? "default",
+
 };
